7ba62d50cc370ce452992c4b73469a491d7d00e4
ashish sood <ashishsood1998@gmail.com>
2026-05-19 13:17:30 -0500

    issues fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import Star from "./components/Star";
-
+import { useState } from "react";
+import StarRating from "./components/StarRating";
+import Dialog from "./components/Dialog";
 
 function App() {
-  <>
-  </>
+  const [showDialog, setShowDialog] = useState(false);
+
+  const openDialog = () => {
+    setShowDialog(true);
+  };
+
+  const closeDialog = () => {
+    setShowDialog(false);
+  };
+
+  return (
+    <main className="wrapBox">
+      <h1 className="mainTitle">Rating System</h1>
+
+      <StarRating title="Movie Review" />
+
+      <button
+        className="openBtn"
+        onClick={openDialog}
+      >
+        Open Dialog
+      </button>
+
+      {showDialog && (
+        <Dialog
+          text="Thanks for visiting our website!"
+          close={closeDialog}
+        />
+      )}
+    </main>
+  );
 }
 
 export default App;
--- src/components/Star.js
+++ src/components/Star.js
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FaStar } from "react-icons/fa";
 
 function Star(props) {
