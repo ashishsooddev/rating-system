643e10e77011fa445c7671a07817ee8fa16e0d28
ashish sood <ashishsood1998@gmail.com>
2026-05-19 13:30:29 -0500

    code issues fixed

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -6,14 +6,24 @@ function StarRating(props) {
 
   const words = [
     "Very Bad",
-    "poor",
+    "Poor",
     "Okay",
     "Good",
     "Excellent"
   ];
 
   return (
-    <></>
+      <section className="ratingArea">
+      <h2 className="ratingTitle">{props.title}</h2>
+      <div className="starRow">
+        {[1, 2, 3, 4, 5].map((item) => (
+          <Star key={item} ok={item <= count} hit={() => setCount(item)}/>
+        ))}
+      </div>
+      <p className="ratingText">
+        {count === 0 ? "Choose Rating" : words[count - 1]}
+      </p>
+    </section>
   );
 }
 
