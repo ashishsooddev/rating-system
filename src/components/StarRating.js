2dcad74466d27d7fecea0333e11f364fe7123697
ashish sood <ashishsood1998@gmail.com>
2026-05-19 13:25:17 -0500

    star rating code updated

---
+++ src/components/StarRating.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Star from "./Star";
+
+function StarRating(props) {
+  const [count, setCount] = useState(0);
+
+  const words = [
+    "Very Bad",
+    "poor",
+    "Okay",
+    "Good",
+    "Excellent"
+  ];
+
+  return (
+    <></>
+  );
+}
+
+export default StarRating;   
